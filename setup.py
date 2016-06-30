ab54b27f46678e037c3dbb3f59c73fc450dd67b0
Toby Patterson <toby@powerhousedynamics.com>
2016-06-30 16:32:37 -0400

    updating setup

--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@ from setuptools import setup
 setup(name='async-dispatch',
       version='0.1',
       description='Dispatches events from a publisher to any number of subscribers.',
+      url='https://github.com/tobypatterson/async-dispatch/',
+      author='Toby Patterson',
+      author_email='me@tobys.email',
+      license='MIT',
       py_modules=['async_dispatch'],
-      setup_requires=[
-          'pytest-runner',
-      ],
       tests_require=[
           'pytest',
       ],
+      classifiers=[
+         'Intended Audience :: Developers',
+         'Programming Language :: Python :: 3.5',
+	 'Topic :: Communications'
+      ],
+      extras_require={
+          ':python_version == "3.5"': ['asyncio']
+      },
 )
