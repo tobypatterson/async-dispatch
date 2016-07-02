b693121b099077d628f6733121ff76fbd785fc3b
Toby Patterson <toby.patterson@gmail.com>
2016-07-02 19:24:54 -0400

    Trying to work with rst.

---
+++ README.rst
@@ -0,0 +1,37 @@
+Async Dispatch
+=============
+
+Python Asyncio-enabled Event Dispatcher Based on Publishers and Subscribers
+
+Build Status
+------------
+
+.. image:: https://travis-ci.org/tobypatterson/async-dispatch.svg?branch=master
+
+Requirements
+------------
+
+Async Dispatch uses the keywords async and await, and thus requires Python 3.5 or greater. This version of Python is distributed in the most recent release of Ubuntu 16.04, and can be easily installed on Mac OS X using brew .
+
+Installation
+------------
+
+The prefered way to install Async Dispatch for Python 3.5 is using pip:. 
+
+.. code:: bash
+
+   pip install async-dispatch
+
+Basic Usage
+-----------
+
+Documentation will soon be available on readthedocs.  Here is a brief summary.
+
+.. code:: python
+   publisher = BasicPublisher()
+   subscriber = BasicSubscriber()
+
+   loop = asyncio.get_event_loop()
+
+   server = Dispatcher(publisher, subscriber, loop=loop)
+   loop.run_until_complete(server.start(max_events=5))
--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup
 
-setup(name='async-dispatch',
-      version='0.1',
-      description='Dispatches events from a publisher to any number of subscribers.',
-      url='https://github.com/tobypatterson/async-dispatch/',
-      author='Toby Patterson',
-      author_email='me@tobys.email',
-      license='MIT',
-      packages=['async_dispatch'],
-      tests_require=[
-          'pytest',
-      ],
-      classifiers=[
-         'Intended Audience :: Developers',
-         'Programming Language :: Python :: 3.5',
-	 'Topic :: Communications'
-      ],
+setup(
+    name='async-dispatch',
+    version='0.1',
+    description='Dispatches events from a publisher to any number of subscribers.',
+    url='https://github.com/tobypatterson/async-dispatch/',
+    download_url='http://github.com/tobypatterson/async-dispatch/tarball/master',
+    author='Toby Patterson',
+    author_email='me@tobys.email',
+    license='MIT',
+    packages=['async_dispatch'],
+    tests_require=[
+        'pytest',
+    ],
+    keywords=['asyncio', 'events', 'queue'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Communications'
+    ],
 )
