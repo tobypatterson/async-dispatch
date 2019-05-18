a5a592960bb9c24bdc4c5e78cd132f60c2326ee2
Thomas Sterren <thomas.sterren@gmail.com>
2019-05-18 09:13:02 +0200

    Added _get_events() to receive dict of event handlers from a subscriber

--- async_dispatch.py
+++ async_dispatch.py
@@ -68,14 +68,7 @@ class Dispatcher:
         if not isinstance(subscriber, SubscriberInterface):
             raise TypeError('Expects an instance of the SubscriberInterface')
 
-        events = subscriber.get_event_listeners()
-        t = type(events)
-        if t is dict:
-            pass
-        elif t is list:
-            events = {event_name: subscriber.consume for event_name in events}
-        else:
-            raise TypeError('Cannot determine events from {}'.format(t))
+        events = self._get_events(subscriber)
 
         for event_name, handler in events.items():
 
@@ -97,14 +90,7 @@ class Dispatcher:
         if not isinstance(subscriber, SubscriberInterface):
             raise TypeError('Expects an instance of the SubscriberInterface')
 
-        events = subscriber.get_event_listeners()
-        t = type(events)
-        if t is dict:
-            pass
-        elif t is list:
-            events = {event_name: subscriber.consume for event_name in events}
-        else:
-            raise TypeError('Cannot determine events from {}'.format(t))
+        events = self._get_events(subscriber)
 
         for event_name, handler in events.items():
             # A string could be the name of the handler
@@ -114,6 +100,28 @@ class Dispatcher:
             self.remove_handler(handler, event_name)
 
         return self
+
+    @staticmethod
+    def _get_events(subscriber):
+        """Returns a dictionary containing the subscribers event handlers.
+
+        :param subscriber: SubscriberInterface
+        :return: dict containing event handlers
+        :rtype: dict[str, str]
+        """
+        if not isinstance(subscriber, SubscriberInterface):
+            raise TypeError('Expects an instance of the SubscriberInterface')
+
+        events = subscriber.get_event_listeners()
+        t = type(events)
+        if t is dict:
+            pass
+        elif t is list:
+            events = {event_name: subscriber.consume for event_name in events}
+        else:
+            raise TypeError('Cannot determine events from {}'.format(t))
+
+        return events
 
     def add_handler( self, handler, event_name):
         """
