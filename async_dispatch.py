2e1b5ed769eaf7195878d4e69fc8f473112b709c
Thomas Sterren <thomas.sterren@gmail.com>
2019-05-18 09:03:33 +0200

    Added remove_subscriber() method

--- async_dispatch.py
+++ async_dispatch.py
@@ -87,7 +87,33 @@ class Dispatcher:
 
         return self
 
+    def remove_subscriber(self, subscriber):
+        """
+        Removes a subscriber and its event handlers.
 
+        :param subscriber: SubscriberInterface
+        :return: self
+        """
+        if not isinstance(subscriber, SubscriberInterface):
+            raise TypeError('Expects an instance of the SubscriberInterface')
+
+        events = subscriber.get_event_listeners()
+        t = type(events)
+        if t is dict:
+            pass
+        elif t is list:
+            events = {event_name: subscriber.consume for event_name in events}
+        else:
+            raise TypeError('Cannot determine events from {}'.format(t))
+
+        for event_name, handler in events.items():
+            # A string could be the name of the handler
+            if isinstance(handler, str) and hasattr(subscriber, handler):
+                handler = getattr(subscriber, handler)
+
+            self.remove_handler(handler, event_name)
+
+        return self
 
     def add_handler( self, handler, event_name):
         """
